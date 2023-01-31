6045daffee41339c20a613da43e4e7a06b5a8e97
Michael Gioeni <gioenijr@oswego.edu>
2023-01-31 04:53:27 -0700

    4 passing

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1 * num2);
+multiply;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+random;
+
+const num3 = 29;
+const num4 = 5;
+let mod = (num3 % num4);
+mod;
+
+
+const max = Math.ceil(20);
+max;
